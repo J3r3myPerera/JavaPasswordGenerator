e0bf1b7a772e460b2cc8089b6f8c28be518d4c70
Dinuka Perera <dinuka.20210433@iit.ac.lk>
2023-06-08 01:28:11 +0530

    Complete the Password class

--- PawordGenerator/src/Password.java
+++ PawordGenerator/src/Password.java
@@ -24,5 +24,59 @@ public class Password {
             val = 3;
         }
 
+        //if its a symbol
+        else {
+            val = 4;
+        }
+
+        return val;
+
+    }
+
+    public int PasswordStrength() {
+        String s = this.Value;
+        boolean UsedUpper = false;
+        boolean UsedLower = false;
+        boolean UsedNum = false;
+        boolean UsedSym = false;
+        int type;
+        int Score =0;
+
+        for (int i=0;  i<s.length(); i++) {
+            char c = s.charAt(i);
+            type = CharType(c);
+
+            if (type == 1) UsedUpper = true;
+            if (type == 2) UsedLower = true;
+            if (type == 3) UsedNum = true;
+            if (type == 4) UsedSym = true;
+        }
+
+        if (UsedUpper) Score += 1;
+        if (UsedLower) Score += 1;
+        if (UsedNum) Score += 1;
+        if (UsedSym) Score += 1;
+
+        return Score;
+
+    }
+
+    public String calculateScore() {
+        int Score = this.PasswordStrength();
+
+        if (Score == 6) {
+            return "Password strength is Strong.";
+        } else if (Score >= 4 ){
+            return "Password strength is good.";
+        } else if (Score >= 3) {
+            return "Password strength is Medium";
+        } else {
+            return "Weak Password!";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Value;
     }
 }
