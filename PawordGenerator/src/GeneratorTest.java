f08a88249651a7c3027b8f142715f30e19287d12
Dinuka Perera <dinuka.20210433@iit.ac.lk>
2023-06-08 02:27:09 +0530

    Main class

--- PawordGenerator/src/GeneratorTest.java
+++ PawordGenerator/src/GeneratorTest.java
@@ -1,2 +1,10 @@
 public class GeneratorTest {
+
+    private final Password password = new Password("Secret");
+    private final Alaphabet firstAlphabet = new Alaphabet(true, false, false, false );
+    private final Alaphabet secondAlphabet = new Alaphabet(false, true, true, true);
+    private final Generator generator = new Generator(true, false, false, false);
+
+    @Test
+    void
 }
